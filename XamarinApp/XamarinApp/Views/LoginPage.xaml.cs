41ebae09bcccc760d321de9517cc4d8dcd1c2032
sourcecode121 <sourcecode121@users.noreply.github.com>
2018-02-11 17:23:07 +0530

    Add project files.

---
+++ XamarinApp/XamarinApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage()
+		{
+			InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+		}
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if(isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+	}
+}
